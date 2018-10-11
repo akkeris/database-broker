23e44c699654622ced4bdac4e5ba757b1b8031ee
Trevor Linton <trevor.linton@gmail.com>
2018-10-11 12:02:12 -0600

    fixed logic in get binding

--- pkg/broker/logic.go
+++ pkg/broker/logic.go
@@ -6,7 +6,6 @@ import (
 	"github.com/golang/glog"
 	osb "github.com/pmorie/go-open-service-broker-client/v2"
 	"github.com/pmorie/osb-broker-lib/pkg/broker"
-	"errors"
 )
 
 type BusinessLogic struct {
@@ -765,7 +764,7 @@ func (b *BusinessLogic) ValidateBrokerAPIVersion(version string) error {
 func (b *BusinessLogic) GetBinding(request *osb.GetBindingRequest, context *broker.RequestContext) (*osb.GetBindingResponse, error) {
 	dbInstance, err := b.GetInstanceById(request.InstanceID)
 	if err == nil && InProgress(dbInstance.Status) {
-		return nil, errors.New("service not yet available")
+		return nil, UnprocessableEntityWithMessage("ServiceNotYetAvailable", "The service requested is not yet available.")
 	}
 	if err != nil && err.Error() == "Cannot find database instance" {
 		return nil, NotFound()
