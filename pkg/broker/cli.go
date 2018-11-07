97ef35c15405c4b940b7249fd55188e0f2a32de1
Trevor Linton <trevor.linton@gmail.com>
2018-11-07 15:06:21 -0700

    COBRA-2206 - screw enums, we dont need em

--- pkg/broker/cli.go
+++ pkg/broker/cli.go
@@ -10,6 +10,6 @@ type Options struct {
 }
 
 func AddFlags(o *Options) {
-	flag.StringVar(&o.DatabaseUrl, "name-prefix", "", "The prefix to use on database names (generally short a few characters), you can also set NAME_PREFIX environment var.")
-	flag.StringVar(&o.NamePrefix, "database-url", "", "The database url to use for storage (e.g., postgres://user:pass@host:port/dbname), you can also set DATABASE_URL environment var.")
+	flag.StringVar(&o.NamePrefix, "name-prefix", "", "The prefix to use on database names (generally short a few characters), you can also set NAME_PREFIX environment var.")
+	flag.StringVar(&o.DatabaseUrl, "database-url", "", "The database url to use for storage (e.g., postgres://user:pass@host:port/dbname), you can also set DATABASE_URL environment var.")
 }
